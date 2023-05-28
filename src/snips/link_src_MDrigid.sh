8df0e8676d682d5c5446b2a53af57b3bda98b0ba
Andrea Silva <andreasilva759@gmail.com>
2023-05-28 10:38:27 +0200

    move snippets to src folder

---
+++ src/snips/link_src_MDrigid.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+src_fld=/PATH/TO/SOURCE/OF/rigid_cluster
+src_fname=( tool_create_cluster.py tool_create_substrate.py misc.py ) # minimal
+if [[ $1 == "all" ]]
+then
+        src_fname=( ${src_fname[@]} MD_rigid_rototrasl.py static_roto_map.py static_trasl_map.py static_rototrasl_map.py static_barrier_string.py string_method.py )
+fi
+
+force_opt=""
+if [[ $2 == "f" ]]
+then
+    echo "Force option enabled"
+    force_opt="-f"
+fi
+
+
+echo "link from $src_fld to $PWD "; 
+for i in ${src_fname[@]}
+do 
+        echo "linking $i"
+        ln $force_opt -s $src_fld/$i ; 
+done
